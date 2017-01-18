198daa18a698682e1101c4450a6dbba64515a112
Ben Carpenter <carpenter@earthlinginteractive.com>
2017-01-18 09:31:33 -0600

    redirection examples

--- client/helloworld.js
+++ client/helloworld.js
@@ -1,5 +1,21 @@
 var myApp = angular.module('helloworld', ['ui.router']);
 
 myApp.config(function($stateProvider) {
-    
+    //example of redirecting to a new state
+    $stateProvider
+        .state('home', {
+            url: '/',
+            redirectTo: () => {
+                return 'hello';
+            }
+        });
+
+    //example of redirecting outside the state tree
+    $stateProvider
+        .state('google', {
+            url: '/google',
+            onEnter: function() {
+                window.location = 'http://www.google.com';
+            }
+        });
 });
